aa1d1c8897ab8cf948dc6d805e24685796c280bc
ramadhan321 <ramadhanadam1104@gmail.com>
2024-08-21 01:14:53 +0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import CardContack from './projeck/CardContack'
+import TicTacTo from './projeck/TicTacTo'
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<CardContack/>}/>
+      <Route path='/' element={<TicTacTo/>}/>
     </Routes>
   )
 }
