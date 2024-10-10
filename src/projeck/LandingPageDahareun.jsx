be896a938e5526f242f7a47bc290ae1e84595332
Angga Adithiya <anggaadithiya@gmail.com>
2024-10-10 20:06:56 +0700

    test commit

--- src/projeck/LandingPageDahareun.jsx
+++ src/projeck/LandingPageDahareun.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import GroupImage from 'src/assets/Group 48095711.png';
+import MaskImage from 'src/assets/Mask group.png';
 
 const LandingPageDahareun = () => {
 
@@ -16,7 +18,7 @@ const LandingPageDahareun = () => {
         </nav>
         <div className={`flex flex-col gap-5 mt-5 items-center`}>
             <h1 className={'text-3xl font-bold font'}>fastes and cheapest food Delivery service then other</h1>
-            <img src="src/assets/Group 48095711.png" alt="" />
+            <img src={GroupImage} alt="" />
             <h1>Get the best quality and most delicious food delivery in the world yiu can get them all at Dahareun</h1>
             <span className='border p-1 flex w-[95%] rounded-lg'>
                 <input type="text" placeholder='Find Restaurant Here' />
@@ -25,9 +27,9 @@ const LandingPageDahareun = () => {
         </div>
         <div className={`flex flex-col gap-5 mt-5 `}>
             <h1>fastes and cheapest food Delivery service then other</h1>
-            <img src="src/assets/Mask group.png" alt="" />
+            <img src={MaskImage} alt="" />
             <h1>Get the best quality and most delicious food delivery in the world yiu can get them all at Dahareun</h1>
-                <button className={`w-[100px] h-[40px] bg-red-500 text-white rounded-lg`}>Get Started</button>
+            <button className={`w-[100px] h-[40px] bg-red-500 text-white rounded-lg`}>Get Started</button>
         </div>
         <h1>Ini adalah teks<span style="display:block;">yang turun ke baris berikutnya</span></h1>
 
